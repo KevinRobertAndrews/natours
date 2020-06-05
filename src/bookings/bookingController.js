488d7d51dbb2002bf12ce8d3026fa468027f333b
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2020-06-05 13:17:42 -0400

    Fix User.findOne in bookingController.js

--- src/bookings/bookingController.js
+++ src/bookings/bookingController.js
@@ -58,7 +58,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await user.findOne({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = sessions.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
